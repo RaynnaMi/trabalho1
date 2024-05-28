d77decff8ab6a57fb8e7fe6b4105af4748b3e81b
RaynnaMi <151586125+RaynnaMi@users.noreply.github.com>
2024-05-27 21:35:53 -0300

    Update dashboard.php

--- f1_gerenciador/dashboard.php
+++ f1_gerenciador/dashboard.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(empty($_SESSION)){
-        print "<script>location.href= 'index.php'</script>";
+        echo "<script>location.href= 'index.php'</script>";
     }
 ?>
 <!DOCTYPE html>
